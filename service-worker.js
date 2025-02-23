fb0a95877f9d7f6c379f9ff8ea829c3c813ceb8a
Joshua Canono <canono.joshua124@gmail.com>
2025-02-23 20:17:05 +0800

    Added Manifestation

--- service-worker.js
+++ service-worker.js
@@ -1,19 +1,39 @@
-self.addEventListener('install', event => {
+const CACHE_NAME = "number-collector-v1";
+const FILES_TO_CACHE = [
+    "./",
+    "./index.html",
+    "./css/style.css",
+    "./script/main.js",
+    "./add-ons/HP_logo.png"
+];
+
+// Install Event - Caching Static Files
+self.addEventListener("install", (event) => {
     event.waitUntil(
-        caches.open('number-collector-cache').then(cache => {
-            return cache.addAll([
-                './index.html',
-                './css/style.css',
-                './script/script.js',
-                './icon.png'
-            ]);
+        caches.open(CACHE_NAME).then((cache) => {
+            console.log("Caching files...");
+            return cache.addAll(FILES_TO_CACHE);
         })
     );
 });
 
-self.addEventListener('fetch', event => {
+// Activate Event - Cleanup Old Caches
+self.addEventListener("activate", (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames
+                    .filter((name) => name !== CACHE_NAME)
+                    .map((name) => caches.delete(name))
+            );
+        })
+    );
+});
+
+// Fetch Event - Serve Cached Files
+self.addEventListener("fetch", (event) => {
     event.respondWith(
-        caches.match(event.request).then(response => {
+        caches.match(event.request).then((response) => {
             return response || fetch(event.request);
         })
     );
